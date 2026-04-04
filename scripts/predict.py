1dd92b2091ea603d5b74eb4b35c06ff06c614078
Bosun4 <64057191+Bosun4@users.noreply.github.com>
2026-04-04 12:47:12 +0800

    更新 predict.py

--- scripts/predict.py
+++ scripts/predict.py
@@ -460,7 +460,7 @@ async def async_call_one_ai_batch(session, prompt, url_env, key_env, models_list
 
 async def run_ai_matrix(prompt, num_matches):
     ai_configs = [
-        ("claude", "CLAUDE_API_URL", "CLAUDE_API_KEY", ["熊猫-特供-A-55-claude-opus-4.6-thinking","熊猫特供-超纯满血-99额度-claude-opus-4.6-thinking","熊猫-按量-特供顶级-官方正向满血-claude-opus-4.6-thinking"]),
+        ("claude", "CLAUDE_API_URL", "CLAUDE_API_KEY", ["熊猫特供-超纯满血-99额度-claude-opus-4.6-thinking","熊猫-按量-特供顶级-官方正向满血-claude-opus-4.6-thinking"]),
         ("grok", "GROK_API_URL", "GROK_API_KEY", ["熊猫-A-7-grok-4.2-多智能体讨论","熊猫-A-6-grok-4.2-thinking"]),
         ("gpt", "GPT_API_URL", "GPT_API_KEY", ["熊猫-A-10-gpt-5.4","熊猫-按量-gpt-5.4","熊猫-A-10-gpt-5.3-codex"]),
         ("gemini", "GEMINI_API_URL", "GEMINI_API_KEY", ["熊猫特供-按量-SSS-gemini-3.1-pro-preview-thinking","熊猫-顶级特供-X-17-gemini-3.1-pro-preview"]),
